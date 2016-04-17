a40c5aa5485a6e9bd5f920d162f0250bb8d56888
Jay Shenoy <jayshenoy@hotmail.com>
2016-04-17 09:13:34 -0400

    Add comment functionality

--- main.py
+++ main.py
@@ -59,7 +59,11 @@ def allowed_file(filename):
 
 @app.route('/')
 def home():
-    return 'Home'
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 @login_required
